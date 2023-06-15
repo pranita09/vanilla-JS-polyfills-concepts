ab6b0b54dabfbd3a6b88bb594c46c63d67d3bed6
Pranita <pfulsundar8@gmail.com>
2023-06-16 04:42:29 +0530

    feat: filter polyfill

--- polyfills/filter/filter.js
+++ polyfills/filter/filter.js
@@ -19,3 +19,17 @@ const filterPolyfill = (arr, cb) => {
 const isEven = (num) => num % 2 === 0;
 
 console.log(filterPolyfill(arr, isEven));
+
+// using Array.prototype
+
+Array.prototype.myFilter = function (cb) {
+  const newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb([this[i]], i, this)) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(arr.myFilter((num, i, arr) => num % 2 === 0));
